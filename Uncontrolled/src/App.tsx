574c7e8a9d035b564935b4e9ebdeba6f8e4c7e23
Alex-DeveloperFS <iotov.developer@gmail.com>
2024-06-16 16:11:08 +0300

    Uncontrolled commit

---
+++ Uncontrolled/src/App.tsx
@@ -0,0 +1,12 @@
+import Checkbox from "./components/Checkbox.tsx"
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Uncontrolled</h1>
+      <Checkbox />
+    </div>
+  )
+}
+
+export default App
